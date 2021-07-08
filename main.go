edbc9e3a8f7cb93c61749be1f60d1c332e2427fc
Rubin Bhandari <roobin.bhandari@gmail.com>
2021-07-08 11:00:39 +0545

    fix: rinit

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gernest/wow"
+	"github.com/gernest/wow/spin"
+	"github.com/manifoldco/promptui"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func writeFile(){
+
+}
+
+
+
+func main() {
+	loadingSpinner := wow.New(os.Stdout, spin.Get(spin.Dots), "Loading")
+	loadingSpinner.Start()
+	resp, err := http.Get("https://www.gitignore.io/api/list")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//We Read the response body on the line below.
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//Convert the body to type string
+	sb := string(body)
+	items := strings.Split(sb,",")
+	loadingSpinner.Stop()
+
+	prompt := promptui.Select{
+		Label: "Select environment",
+		Items: items,
+		StartInSearchMode : true,
+		Searcher : func(input string, index int) bool {
+		pepper := items[index]
+		name := strings.Replace(strings.ToLower(pepper), " ", "", -1)
+		input = strings.Replace(strings.ToLower(input), " ", "", -1)
+
+		return strings.Contains(name, input)
+	},
+	}
+
+	_, result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	addingSpinner := wow.New(os.Stdout, spin.Get(spin.Dots), "Adding gitignore")
+
+	giResponse, err := http.Get(fmt.Sprintf("https://www.gitignore.io/api/%s",result))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//We Read the response body on the line below.
+	giBody, err := ioutil.ReadAll(giResponse.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//Convert the body to type string
+	addingSpinner.Stop()
+	fmt.Println(string(giBody))
+
+
+}
